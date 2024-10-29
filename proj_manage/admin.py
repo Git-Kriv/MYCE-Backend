d1243537342a25c881c961f2b7047c101df92052
srbh001 <22B3905@iitb.ac.in>
2024-10-29 12:08:03 +0530

    Add admin views.

--- proj_manage/admin.py
+++ proj_manage/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from .models import ArchitectureDesign, SellingProperty, BuyingProperty
 
-# Register your models here.
+
+@admin.register(ArchitectureDesign)
+class ArchitectureDesignAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "land_size",
+        "requirement_type",
+        "digital_survey",
+    )
+    search_fields = ("location_line_1", "location_line_2", "requirement_type")
+    list_filter = ("requirement_type", "digital_survey")
+    ordering = ("location_line_1",)
+
+
+@admin.register(SellingProperty)
+class SellingPropertyAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "property_type",
+        "land_size",
+        "expected_price",
+    )
+    search_fields = ("location_line_1", "location_line_2", "property_type")
+    list_filter = ("property_type",)
+    ordering = ("location_line_1",)
+
+
+@admin.register(BuyingProperty)
+class BuyingPropertyAdmin(admin.ModelAdmin):
+    list_display = (
+        "location_line_1",
+        "user",
+        "property_type",
+        "land_size",
+        "budget",
+    )
+    search_fields = ("location_line_1", "location_line_2", "property_type")
+    list_filter = ("property_type",)
+    ordering = ("location_line_1",)
--- utils/admin.py
+++ utils/admin.py
@@ -1,6 +1,33 @@
 from django.contrib import admin
+from .models import House, IndustrialProperty, CommercialProperty, Inquiries
 
-from utils.models import House, IndustrialProperty
 
-admin.site.register(House)
-admin.site.register(IndustrialProperty)
+@admin.register(House)
+class HouseAdmin(admin.ModelAdmin):
+    list_display = ("home_type", "plan_details", "digital_survey", "user")
+    search_fields = ("home_type", "plan_details")
+    list_filter = ("home_type", "digital_survey")
+    ordering = ("home_type",)
+
+
+@admin.register(IndustrialProperty)
+class IndustrialPropertyAdmin(admin.ModelAdmin):
+    list_display = ("property_type", "plan_details", "digital_survey", "user")
+    search_fields = ("property_type", "plan_details")
+    list_filter = ("property_type", "digital_survey")
+    ordering = ("property_type",)
+
+
+@admin.register(CommercialProperty)
+class CommercialPropertyAdmin(admin.ModelAdmin):
+    list_display = ("cp_type", "plan_details", "digital_survey", "user")
+    search_fields = ("cp_type", "plan_details")
+    list_filter = ("cp_type", "digital_survey")
+    ordering = ("cp_type",)
+
+
+@admin.register(Inquiries)
+class InquiriesAdmin(admin.ModelAdmin):
+    list_display = ("email", "phone_number", "message")
+    search_fields = ("email", "phone_number", "message")
+    ordering = ("email", "phone_number")
