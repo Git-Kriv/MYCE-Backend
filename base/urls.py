784b8aefcab5db549cd9786f8a943c00a5807230
srbh001 <22B3905@iitb.ac.in>
2024-11-29 19:09:47 +0530

    Minor fix

--- base/urls.py
+++ base/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -21,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/", include("user_auth.urls")),
-    path("api/", include("utils.urls"))
+    path("api/", include("utils.urls")),
+    path("api/", include("proj_manage.urls")),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
